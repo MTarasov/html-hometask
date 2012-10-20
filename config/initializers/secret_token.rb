8d51cd6a6c4b1dce8097cad521e361bf0e8cc945
MTarasov <dragon212@yandex.ru>
2012-10-20 21:50:56 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HtmlHometask::Application.config.secret_token = '6920b0be4378d13b99103dc02cc853dfac81f41b32e5749d5b3b5fbb586f59131f8403cd6e3c0e05f93cb06e37d6d2e8ac294a12d85633d6bc83166ec71bcaf4'
